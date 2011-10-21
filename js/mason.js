5fb22a506046c0d431b03234185939a73a153dc4
Jason Butz <jasonbutz@gmail.com>
2011-10-21 16:42:37 -0500

    Mason/pigpen cipher stuff seems to be working

--- js/mason.js
+++ js/mason.js
@@ -1,147 +1,184 @@
-charH = 20;
-charW = 20;
 var Mason = {
 	charW: 20,
 	charH: 20,
 	letterMap: {
 		a: [false,[
-				[0,charH],
-				[charW,charH],
-				[charW,0]]
+				[0,20],
+				[20,20],
+				[20,0]]
 			],
 		b: [false,[
 				[0,0],
-				[0,charH],
-				[charW,charH],
-				[charW,0]]
+				[0,20],
+				[20,20],
+				[20,0]]
 			],
 		c: [false,[
 				[0,0],
-				[0,charH],
-				[charW,charH]]
+				[0,20],
+				[20,20]]
 			],
 		d: [false,[
 				[0,0],
-				[charW,0],
-				[charW,charH],
-				[0,charH]]
+				[20,0],
+				[20,20],
+				[0,20]]
 			],
 		e: [false,[
 				[0,0],
-				[0,charH],
-				[charW,charH],
-				[charW,0]]
+				[0,20],
+				[20,20],
+				[20,0]]
 			],
 		f: [false,[
-				[charW,0],
+				[20,0],
 				[0,0],
-				[0,charH],
-				[charW,charH]]
+				[0,20],
+				[20,20]]
 			],
 		g: [false,[
 				[0,0],
-				[charW,0],
-				[charW,charH]]
+				[20,0],
+				[20,20]]
 			],
 		h: [false,[
-				[0,charH],
+				[0,20],
 				[0,0],
-				[charW,0],
-				[charW,charH]]
+				[20,0],
+				[20,20]]
 			],
 		i: [false,[
-				[0,charH],
+				[0,20],
 				[0,0],
-				[charW,0]]
+				[20,0]]
 			],
 		j: [true,[
-				[0,charH],
-				[charW,charH],
-				[charW,0]]
+				[0,20],
+				[20,20],
+				[20,0]]
 			],
 		k: [true,[
 				[0,0],
-				[0,charH],
-				[charW,charH],
-				[charW,0]]
+				[0,20],
+				[20,20],
+				[20,0]]
 			],
 		l: [true,[
 				[0,0],
-				[0,charH],
-				[charW,charH]]
+				[0,20],
+				[20,20]]
 			],
 		m: [true,[
 				[0,0],
-				[charW,0],
-				[charW,charH],
-				[0,charH]]
+				[20,0],
+				[20,20],
+				[0,20]]
 			],
 		n: [true,[
 				[0,0],
-				[0,charH],
-				[charW,charH],
-				[charW,0]]
+				[0,20],
+				[20,20],
+				[20,0]]
 			],
 		o: [true,[
-				[charW,0],
+				[20,0],
 				[0,0],
-				[0,charH],
-				[charW,charH]]
+				[0,20],
+				[20,20]]
 			],
 		p: [true,[
 				[0,0],
-				[charW,0],
-				[charW,charH]]
+				[20,0],
+				[20,20]]
 			],
 		q: [true,[
-				[0,charH],
+				[0,20],
 				[0,0],
-				[charW,0],
-				[charW,charH]]
+				[20,0],
+				[20,20]]
 			],
 		r: [true,[
-				[0,charH],
+				[0,20],
 				[0,0],
-				[charW,0]]
+				[20,0]]
 			],
 		s: [false,[
 				[0,0],
-				[Math.floor(charW / 2),charH],
-				[charW, 0]]
+				[Math.floor(20 / 2),20],
+				[20, 0]]
 			],
 		t: [false,[
 				[0,0],
-				[charW,Math.floor(charH / 2)],
-				[0,charH]]
+				[20,Math.floor(20 / 2)],
+				[0,20]]
 			],
 		u: [false,[
-				[charW,0],
-				[0,Math.floor(charH / 2)],
-				[charW,charH]]
+				[20,0],
+				[0,Math.floor(20 / 2)],
+				[20,20]]
 			],
 		v: [false,[
-				[0,charH],
-				[Math.floor(charW / 2), charH]]
+				[0,20],
+				[Math.floor(20 / 2), 20]]
 			],
 		w: [true,[
 				[0,0],
-				[Math.floor(charW / 2),charH],
-				[charW, 0]]
+				[Math.floor(20 / 2),20],
+				[20, 0]]
 			],
 		x: [true,[
 				[0,0],
-				[charW,Math.floor(charH / 2)],
-				[0,charH]]
+				[20,Math.floor(20 / 2)],
+				[0,20]]
 			],
 		y: [true,[
-				[charW,0],
-				[0,Math.floor(charH / 2)],
-				[charW,charH]]
+				[20,0],
+				[0,Math.floor(20 / 2)],
+				[20,20]]
 			],
 		z: [true,[
-				[0,charH],
-				[Math.floor(charW / 2), charH]]
+				[0,20],
+				[Math.floor(20 / 2), 20]]
 			]
+	},
+	init: function(charW, charH)
+	{
+		if(charW < 20 || charH < 20)
+		{
+			throw "Pigpen Cipher character space too small!";
+			return;
+		}
+		this.charW = charW;
+		this.charH = charH;
+		
+		this.letterMap = {
+			a: [false,[[0,charH],[charW,charH],[charW,0]]],
+			b: [false,[[0,0],[0,charH],[charW,charH],[charW,0]]],
+			c: [false,[[0,0],[0,charH],[charW,charH]]],
+			d: [false,[[0,0],[charW,0],[charW,charH],[0,charH]]],
+			e: [false,[[0,0],[0,charH],[charW,charH],[charW,0]]],
+			f: [false,[[charW,0],[0,0],[0,charH],[charW,charH]]],
+			g: [false,[[0,0],[charW,0],[charW,charH]]],
+			h: [false,[[0,charH],[0,0],[charW,0],[charW,charH]]],
+			i: [false,[[0,charH],[0,0],[charW,0]]],
+			j: [true,[[0,charH],[charW,charH],[charW,0]]],
+			k: [true,[[0,0],[0,charH],[charW,charH],[charW,0]]],
+			l: [true,[[0,0],[0,charH],[charW,charH]]],
+			m: [true,[[0,0],[charW,0],[charW,charH],[0,charH]]],
+			n: [true,[[0,0],[0,charH],[charW,charH],[charW,0]]],
+			o: [true,[[charW,0],[0,0],[0,charH],[charW,charH]]],
+			p: [true,[[0,0],[charW,0],[charW,charH]]],
+			q: [true,[[0,charH],[0,0],[charW,0],[charW,charH]]],
+			r: [true,[[0,charH],[0,0],[charW,0]]],
+			s: [false,[[0,0],[Math.floor(charW / 2),charH],[charW, 0]]],
+			t: [false,[[0,0],[charW,Math.floor(charH / 2)],[0,charH]]],
+			u: [false,[[charW,0],[0,Math.floor(charH / 2)],[charW,charH]]],
+			v: [false,[[0,charH],[Math.floor(charW / 2), charH]]],
+			w: [true,[[0,0],[Math.floor(charW / 2),charH],[charW, 0]]],
+			x: [true,[[0,0],[charW,Math.floor(charH / 2)],[0,charH]]],
+			y: [true,[[charW,0],[0,Math.floor(charH / 2)],[charW,charH]]],
+			z: [true,[[0,charH],[Math.floor(charW / 2), charH]]]
+		};
 	},
 	draw: function(l, c, sW, sH, drawLetter)
 	{
@@ -164,15 +201,15 @@ var Mason = {
 		c.stroke();
 		if(hasDot)
 		{
-			c.fillRect(Math.floor(charW / 2) + sW - 2, Math.floor(charH / 2) + sH - 2, 4, 4);
-			fudge = 5;
+			fudge = Math.floor((this.charW + this.charH) / 4);
+			c.fillRect(Math.floor(this.charW / 2) + sW - 2, Math.floor(this.charH / 2) + sH - 2, 4, 4);
 		}
 		
 		if (drawLetter)
 		{
 			c.textBaseline = "middle";
 			c.textAlign = "center";
-			c.fillText(l, Math.floor(charW / 2) + sW + fudge, Math.floor(charH / 2) + sH);
+			c.fillText(l, Math.floor(this.charW / 2) + sW + fudge, Math.floor(this.charH / 2) + sH);
 		}
 	}
 }
@@ -181,10 +218,11 @@ function testDraw()
 {
 	var canvas = document.getElementById("masonc");
 	var context= canvas.getContext("2d");
-	Mason.draw('a',context,0,0,true);
-	Mason.draw('b',context,30,0,true);
-	Mason.draw('c',context,60,0,true);
-	Mason.draw('j',context,0,60,true);
-	Mason.draw('s',context,30,60,true);
-	Mason.draw('y',context,60,60,true);
+
+	var m = Mason;
+	m.init(50,50);
+	m.draw('a',context,0,0,false);
+	m.draw('j',context,60,00,false);
+	m.draw('s',context,0,60,false);
+	m.draw('y',context,60,60,false);
 }
